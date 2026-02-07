6532c62dc9427c4bf47b3759368e56628a811d58
sirij3 <siriyadalam@gmail.com>
2026-02-07 11:40:30 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,12 @@
+public class student {
+    public static void main(String[] args){
+    int total,Avg;
+    int s1=100;
+    int s2=98;
+    int s3=95;
+    total=s1+s2+s3;
+    Avg=s1+s2+s3/3;
+    System.out.println("Total="+total);
+    System.out.println("Average="+Avg);
+    }
+}
