e5ef1387be42713ac09e37be084bdf302fbc4202
sirij3 <siriyadalam@gmail.com>
2026-02-07 10:56:31 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,19 @@
+public class primitive {
+    public static void main(String[] args){
+        byte a=10;
+        short b=120;
+        int c=10000;
+        float d=10.00f;
+        double e=15.888;
+        char f='A';
+        boolean value=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(value);
+    }
+    
+}
