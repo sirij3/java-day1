3497ed386d08b56dfd36d4b21c9c0f7a0b51d255
sirij3 <siriyadalam@gmail.com>
2026-02-07 11:41:03 +0530

    Add files via upload

---
+++ details.java
@@ -0,0 +1,16 @@
+public class details {
+    public static void main(String[] args){
+        String name="nick";
+        int age=18;
+        char grade='A';
+        int marks=98;
+        String status="pass";
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Grade:"+grade);
+        System.out.println("Marks:"+marks);
+        System.out.println("Status:"+status);
+
+    }
+    
+}
