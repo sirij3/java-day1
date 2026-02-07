a41728d6f239faa7ccac3d8dec2e1cb79ae80852
sirij3 <siriyadalam@gmail.com>
2026-02-07 14:53:12 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,18 @@
+public class arithmetic {
+    public static void main(String[] args)
+    {
+        int a=10,b=25;
+        int sum=a+b;
+        System.out.println(sum);
+        if(a>0 && a%2==0)
+            System.out.println("positive and even");
+        else
+            System.out.println("negative");
+        System.out.println(a+=b);
+        System.out.println(a++);
+        System.out.println(a);
+        
+
+    }
+    
+}
