9d711ad24c58c726da01a185f9ac115e9331a9c7
sirij3 <siriyadalam@gmail.com>
2026-02-28 14:15:33 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class sum 
+{
+    public static void main(String[] args)
+    {
+        int arr[]={10,20,30};
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        sum +=arr[i];
+        System.out.println("Sum:"+sum);
+
+    }   
+}
