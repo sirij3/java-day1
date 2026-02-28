7c5f85eb4565712477c60e843c03ed31cf6f69f0
sirij3 <siriyadalam@gmail.com>
2026-02-28 14:16:52 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,24 @@
+public class pattern3
+ {
+    public static void main(String[] args)
+    {
+        int i,j;
+        int n=5;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*"+" ");
+
+            }
+             System.out.println("");
+        }
+
+    }
+}
+
+
